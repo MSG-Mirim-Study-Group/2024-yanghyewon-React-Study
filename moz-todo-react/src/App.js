dad34f6aad19ccb40c18c37e5610ec92aa55b41f
hyewon4052 <s2311@e-mirim.hs.kr>
2024-01-04 23:57:24 +0900

    작업완료

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -2,17 +2,32 @@ import React, { useState } from "react";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
+import { nanoid } from "nanoid";
 
 
 function App(props) {
+
+  const [tasks, setTasks] = useState(props.tasks);
+
+  function addTask(name) {
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+  
+
   const name = "양혜원";
 
-  const taskList = (props.tasks || []).map((task) => (
+  function toggleTaskCompleted(id) {
+    console.log(tasks[0]);
+  }
+  
+  const taskList = tasks.map((task) => (
     <Todo
       id={task.id}
       name={task.name}
       completed={task.completed}
       key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
     />
   ));
 
@@ -22,15 +37,18 @@ function App(props) {
     />
   ));
 
-  
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
+
   return (
     <div className="todoapp stack-large">
       <h1>{name} 컴포넌트화 완성</h1>
-      <Form />
+      <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
         {doneList}
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
